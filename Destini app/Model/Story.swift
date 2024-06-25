0bdd1d6972a1615fb6efb140a29df1a051424b7a
sangambansode45 <bansodesangam23@gmail.com>
2024-06-25 09:32:26 +0530

    Add files via upload

---
+++ Destini app/Model/Story.swift
@@ -0,0 +1,20 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+
+
+import Foundation
+
+
+struct Story {
+
+   var title : String
+   var choice1 : String
+    var choice1Destination : Int
+   var choice2 : String
+    var choice2Destination : Int
+  
+    
+    
+}
+
